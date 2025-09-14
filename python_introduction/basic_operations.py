e1125f40bd490e600bb4561959b45a792b12fb9a
yrnmalone0 <obedbadu04@gmail.com>
2025-09-14 13:24:58 +0000

    added arithmetic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+sum = int (number1 + number2)
+difference = int(number1 - number2)
+product = int(number1 * number2)
+
+print("Addition of " + str(number1) + " and " + str(number2) + " is " + str(sum))
+print("Subtraction of " + str(number1) + " and " + str(number2) + " is " + str(difference))
+print("Multiplication of " + str(number1) + " and " + str(number2) + " is " + str(product))
