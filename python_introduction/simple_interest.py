ee5e83d014aeafa2dee6b4f48b85411236b970a9
yrnmalone0 <obedbadu04@gmail.com>
2025-09-14 14:06:28 +0000

    calculate simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+#SIMPLE INTEREST CALCULATOR
+
+#Defining three(3) variables with values
+principal = 1000
+rate = 0.05
+time = 3
+
+interest = (int(principal) * float(rate) * int(time))
+print("The simple interest is: " + str(interest))
