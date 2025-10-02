c9d3219210edc965fc138c0c9cc2ba6f5e813773
yrnmalone0 <obedbadu04@gmail.com>
2025-10-02 04:42:17 +0000

    made changes to input fields

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -15,13 +15,13 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            item = input("Enter item to add: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
             print(f"{item} added successfully!")
             pass
         elif choice == '2':
             # Prompt for and remove an item
-            item = input("Enter item to remove: ")
+            item = input("Enter the item to remove: ")
             if item in shopping_list:
                 shopping_list.remove(item)
                 print(f"{item} removed successfully!")
