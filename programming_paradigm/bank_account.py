6a48faf6938f799f3b61b39a564970769736ff0c
yrnmalone0 <obedbadu04@gmail.com>
2025-10-05 23:26:05 +0000

    implemented a BankAccount class that encapsulates banking operations

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,27 @@
+# Define a class named BankAccount.
+class BankAccount:
+
+    # Using __init__ method to initialize account_balance attribute. Optionally, accept an initial balance parameter, defaulting to zero
+
+    def __init__(self, account_balance, intial_balance=0):
+        self.account_balance = account_balance
+        self.initial_balance = intial_balance
+
+    # Deposit Method to add the specified amount to account_balance
+    def deposit(self, amount):
+        if amount > 0:
+            return self.account_balance + amount
+
+    # withdraw Method to deduct the amount from account_balance if funds are sufficient
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.account_balance:
+            return self.account_balance - amount
+        
+
+    # display_balance prints the current balance in a user-friendly format
+    def display_balance(self):
+        return f"Current balance: ${self.account_balance:.2f}"
+    
+    # def __str__(self):
+    #     return f"BankAccount(balance={self.account_balance})"
+    
