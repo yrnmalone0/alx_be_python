172fa5592ec041f01226585e68a269dd490ec20b
yrnmalone0 <obedbadu04@gmail.com>
2025-10-19 03:37:07 +0000

    treated class and static methods

--- oop/main.py
+++ oop/main.py
@@ -46,17 +46,35 @@
 
 
 ## For Task 2: Polymorphism Demonstration ##
-from polymorphism_demo import Shape, Rectangle, Circle
-import math
+# from polymorphism_demo import Shape, Rectangle, Circle
+# import math
+
+# def main():
+#     shapes = [
+#         Rectangle(10, 5),
+#         Circle(7)
+#     ]
+
+#     for shape in shapes:
+#         print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
+
+# if __name__ == "__main__":
+#     main()
+
+
+
+
+## For Task 3: Static and Class Methods Demonstration ##
+from class_static_methods_demo import Calculator
 
 def main():
-    shapes = [
-        Rectangle(10, 5),
-        Circle(7)
-    ]
+    # Using the static method
+    sum_result = Calculator.add(10, 5)
+    print(f"The sum is: {sum_result}")
 
-    for shape in shapes:
-        print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
+    # Using the class method
+    product_result = Calculator.multiply(10, 5)
+    print(f"The product is: {product_result}")
 
 if __name__ == "__main__":
     main()
